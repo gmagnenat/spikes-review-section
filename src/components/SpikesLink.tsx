48ccb54ff5f3023f5b072e809c00aca3000606e1
Gwenaël Magnenat <g.magnenat@gmail.com>
2024-12-03 11:55:33 +0100

    Add arrow in link, add eslint rule for escaped entitties

--- src/components/SpikesLink.tsx
+++ src/components/SpikesLink.tsx
@@ -2,9 +2,9 @@ const SpikesLink = () => {
   return (
     <a
       href="https://www.spikes.dev"
-      className="px-4 py-2 rounded-full outline outline-1 outline-[#4C3182] backdrop-blur-sm transition-all ease-in-out focus:outline-1 focus:outline-white hover:outline-white text-base"
+      className="hover:scale-105 px-4 py-2 rounded-full outline outline-1 outline-[#4C3182] backdrop-blur-sm transition-all ease-in-out focus:outline-1 focus:outline-white hover:outline-white text-base"
     >
-      www.spikes.dev
+      www.spikes.dev <span aria-hidden="true">-&gt;</span>
     </a>
   );
 };
